35aea13e0fc572ff81da4f83f599dab70d296946
Andreea Ursachi <dr.ursachi@gmail.com>
2023-06-12 16:43:18 +0300

    added new file

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+import random
+
+print("Welcome to Hangman's game and good luck!")
+print("*" * 26)
+with open("hangman.json", "r") as f:
+    json_object = json.loads(f.read())
+    life = json_object["life"]
+    words = json_object["words"]
+    chosen_word = random.choice(words)
+
+display = []
+
+for i in range(len(chosen_word)):
+    display += "_"
+print(display)
+game_over = False
+while not game_over:
+    guessed_letter = input("Please guess a letter:").lower()
+    for position in range(len(chosen_word)):
+        letter = chosen_word[position]
+        if letter == guessed_letter:
+            display[position] = guessed_letter
+
+    print(display)
+    if guessed_letter not in chosen_word:
+        life -= 1
+        print("The word does not contain the chosen letter")
+        if life == 0:
+            game_over = True
+            print("Game over!")
+    if "_" not in display:
+        print(f"You won, the guessed word was {chosen_word}.")
+        break
